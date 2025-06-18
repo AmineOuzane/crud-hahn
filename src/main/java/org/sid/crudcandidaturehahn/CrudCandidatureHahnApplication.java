98a2e2ae050552a7289b84025e1e08406ff17bbb
Amine <amineouzane10@gmail.com>
2025-06-18 21:40:37 +0100

    creation projet

---
+++ src/main/java/org/sid/crudcandidaturehahn/CrudCandidatureHahnApplication.java
@@ -0,0 +1,13 @@
+package org.sid.crudcandidaturehahn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCandidatureHahnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCandidatureHahnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/crudcandidaturehahn/CrudCandidatureHahnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.crudcandidaturehahn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCandidatureHahnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
